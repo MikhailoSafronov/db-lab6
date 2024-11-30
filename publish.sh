63d6e2bc10f9ce4907b198addb640757d294429c
Mikhailo <144545017+MikhailoSafronov@users.noreply.github.com>
2024-11-30 03:41:17 +0200

    Update publish.sh

--- publish.sh
+++ publish.sh
@@ -1,4 +1,3 @@
-
 #!/usr/bin/env sh
 
 # abort on errors
@@ -8,7 +7,11 @@ set -e
 npm run docs:build
 
 # navigate into the build output directory
+
 cd docs/.vuepress/dist
+
+# Disable Jekyll
+touch .nojekyll
 
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
@@ -24,4 +27,3 @@ git commit -m 'deploy'
 git push -f https://github.com/MikhailoSafronov/db-lab6.git master:gh-pages
 
 cd -
-
