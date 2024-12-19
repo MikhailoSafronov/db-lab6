fd3098cc57005cc5292d9e67f9e2e91e1c2eaba3
Mikhailo <144545017+MikhailoSafronov@users.noreply.github.com>
2024-12-19 20:43:19 +0200

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,11 +1,13 @@
+// .vuepress/config.js
 module.exports = {
   plugins: [
-    '@vuepress/back-to-top',
+    '@vuepress/back-to-top', 
     '@vuepress/pwa', 
     {
       serviceWorker: true,
       updatePopup: true
     },
+    ,
     ['container', {
       type: 'vue',
       before: '<pre class="vue-container"><code>',
@@ -14,73 +16,100 @@ module.exports = {
   ],
   port: 3030,
   base: '/db-lab6/',
-  theme: 'cool',
+  theme: 'vuepress-theme-cool',
+  // dest: 'dist',
   head: [
     ['link', { rel: 'icon', href: '/favicon.ico' }],
     ['link', { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.5.1/katex.min.css' }],
-    ['link', { href: 'https://fonts.googleapis.com/icon?family=Material+Icons', rel: 'stylesheet' }],
+    ['link', {href: 'https://fonts.googleapis.com/icon?family=Material+Icons', rel :'stylesheet'}],
+    
   ],
   themeConfig: {
+    // logo: './myAvatar.png',
     sidebar: [
-      {
-        title: 'Вступ',
-        path: '/intro/',
-      },
-      {
-        title: 'Розроблення загальних вимог до системи',
-        path: '/requirements/',
-        children: [
-          '/requirements/state-of-the-art.md',
-          '/requirements/stakeholders-needs.md',
-        ],
-      },
-      {
-        title: 'Розроблення вимог до функціональності системи',
-        path: '/use-cases/', // Назва папки змінена на use-cases для уникнення проблем із пробілами
-      },
-      {
-        title: 'Проектування інформаційного забезпечення',
-        path: '/design/',
-      },
-      {
-        title: 'Реалізація інформаційного та програмного забезпечення',
-        path: '/software/',
-      },
-      {
-        title: 'Тестування працездатності системи',
-        path: '/test/',
-      },
-      {
-        title: 'Висновки',
-        path: '/conclusion/',
-      },
-    ],
+    {
+      title: 'Вступ',
+      path:"/intro/"
+    },
+    {
+      title: 'Розроблення загальних вимог до системи',
+        path:"/requirements/",
+        children:[
+          '/requirements/state-of-the-art',
+          '/requirements/stakeholders-needs',
+        ]
+      
+    },
+
+    {
+      title: 'Розроблення вимог до функціональности системи',
+      path:"/use cases/"
+    },
+
+    {
+      title: 'Проектування інформаційного забезпечення',
+      path:"/design/"
+    },
+
+    {
+      title: 'Реалізація інформаційного та програмного забезпечення',
+      path:"/software/"
+    },
+
+    {
+      title: 'Тестування працездатності системи',
+      path:"/test/"
+    },
+    {
+      title: 'Висновки',
+      path:"/conclusion/"
+    }
+
+  ],
     sidebarDepth: 2,
-    displayAllHeaders: true,
+    displayAllHeaders: true, // Default: false
+
+
     nav: [
       { text: 'Початок', link: '/' },
     ],
-    lastUpdated: 'Останнє оновлення',
+
+    lastUpdated: 'Останнє оновлення', // string | boolean
+      // Assumes GitHub. Can also be a full GitLab url.
     repo: 'https://github.com/boldak/edu-db-cw',
+    // Customising the header label
+    // Defaults to "GitHub"/"GitLab"/"Bitbucket" depending on `themeConfig.repo`
     repoLabel: 'Github',
+
+    // Optional options for generating "Edit this page" link
+
+    // if your docs are in a different repo from your main project:
+    // docsRepo: 'boldak/dis-edu',
+    // if your docs are not at the root of the repo:
     docsDir: 'docs',
+    // if your docs are in a specific branch (defaults to 'master'):
     docsBranch: 'master',
+    // defaults to false, set to true to enable
+    // editLinks: true,
+    // custom text for edit link. Defaults to "Edit this page"
+    // editLinkText: 'Ви можете покращити цю сторінку'
+
   },
   title: 'Система управління відкритими даними',
   description: 'Курсова робота з дисципліни "Бази даних"',
   configureWebpack: {
     resolve: {
       alias: {
-        '@alias': '../img',
-      },
-    },
+        '@alias': '../img'
+      }
+    }
   },
   markdown: {
     extendMarkdown: md => {
-      md.set({ html: true });
-      md.use(require('markdown-it-katex'));
-      md.use(require('markdown-it-plantuml'));
-      md.use(require('markdown-it-admonition'));
-    },
-  },
-};
+      md.set({ html: true })
+      md.use(require('markdown-it-katex'))
+      md.use(require('markdown-it-plantuml'))
+      md.use(require('markdown-it-admonition'))
+    }
+  }
+}
