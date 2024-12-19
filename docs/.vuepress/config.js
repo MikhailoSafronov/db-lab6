cc02d0202c9b117755b304a9824e294706a0edcf
Mikhailo <144545017+MikhailoSafronov@users.noreply.github.com>
2024-12-19 21:06:45 +0200

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,13 +1,11 @@
-// .vuepress/config.js
 module.exports = {
   plugins: [
-    '@vuepress/back-to-top', 
+    '@vuepress/back-to-top',
     '@vuepress/pwa', 
     {
       serviceWorker: true,
       updatePopup: true
     },
-    ,
     ['container', {
       type: 'vue',
       before: '<pre class="vue-container"><code>',
@@ -15,101 +13,74 @@ module.exports = {
     }]
   ],
   port: 3030,
-  base: '/db-lab6/',
+  base: '/db-lab6/', // Переконайтеся, що це співпадає з назвою репозиторія
   theme: 'vuepress-theme-cool',
-  // dest: 'dist',
   head: [
     ['link', { rel: 'icon', href: '/favicon.ico' }],
     ['link', { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.5.1/katex.min.css' }],
-    ['link', {href: 'https://fonts.googleapis.com/icon?family=Material+Icons', rel :'stylesheet'}],
-    
+    ['link', { href: 'https://fonts.googleapis.com/icon?family=Material+Icons', rel: 'stylesheet' }],
   ],
   themeConfig: {
-    // logo: './myAvatar.png',
     sidebar: [
-    {
-      title: 'Вступ',
-      path:"/intro/"
-    },
-    {
-      title: 'Розроблення загальних вимог до системи',
-        path:"/requirements/",
-        children:[
+      {
+        title: 'Вступ',
+        path: '/intro/', // Шлях до файлу README.md у папці docs/intro
+      },
+      {
+        title: 'Розроблення загальних вимог до системи',
+        path: '/requirements/',
+        children: [
           '/requirements/state-of-the-art',
           '/requirements/stakeholders-needs',
-        ]
-      
-    },
-
-    {
-      title: 'Розроблення вимог до функціональности системи',
-      path:"/use cases/"
-    },
-
-    {
-      title: 'Проектування інформаційного забезпечення',
-      path:"/design/"
-    },
-
-    {
-      title: 'Реалізація інформаційного та програмного забезпечення',
-      path:"/software/"
-    },
-
-    {
-      title: 'Тестування працездатності системи',
-      path:"/test/"
-    },
-    {
-      title: 'Висновки',
-      path:"/conclusion/"
-    }
-
-  ],
-    sidebarDepth: 2,
-    displayAllHeaders: true, // Default: false
-
-
-    nav: [
-      { text: 'Початок', link: '/' },
+        ],
+      },
+      {
+        title: 'Розроблення вимог до функціональности системи',
+        path: '/use-cases/', // Виправлено назву папки
+      },
+      {
+        title: 'Проектування інформаційного забезпечення',
+        path: '/design/',
+      },
+      {
+        title: 'Реалізація інформаційного та програмного забезпечення',
+        path: '/software/',
+      },
+      {
+        title: 'Тестування працездатності системи',
+        path: '/test/',
+      },
+      {
+        title: 'Висновки',
+        path: '/conclusion/',
+      },
     ],
-
-    lastUpdated: 'Останнє оновлення', // string | boolean
-      // Assumes GitHub. Can also be a full GitLab url.
-    repo: 'https://github.com/boldak/edu-db-cw',
-    // Customising the header label
-    // Defaults to "GitHub"/"GitLab"/"Bitbucket" depending on `themeConfig.repo`
-    repoLabel: 'Github',
-
-    // Optional options for generating "Edit this page" link
-
-    // if your docs are in a different repo from your main project:
-    // docsRepo: 'boldak/dis-edu',
-    // if your docs are not at the root of the repo:
-    docsDir: 'docs',
-    // if your docs are in a specific branch (defaults to 'master'):
-    docsBranch: 'master',
-    // defaults to false, set to true to enable
-    // editLinks: true,
-    // custom text for edit link. Defaults to "Edit this page"
-    // editLinkText: 'Ви можете покращити цю сторінку'
-
+    sidebarDepth: 2,
+    displayAllHeaders: true, // Відображати всі заголовки у sidebar
+    nav: [
+      { text: 'Початок', link: '/' }, // Головна сторінка
+    ],
+    lastUpdated: 'Останнє оновлення', // Відображати час останнього оновлення
+    repo: 'https://github.com/MikhailoSafronov/db-lab6', // Посилання на репозиторій
+    repoLabel: 'GitHub', // Текст для кнопки репозиторія
+    docsDir: 'docs', // Директорія документації
+    docsBranch: 'master', // Гілка документації
   },
   title: 'Система управління відкритими даними',
   description: 'Курсова робота з дисципліни "Бази даних"',
   configureWebpack: {
     resolve: {
       alias: {
-        '@alias': '../img'
-      }
-    }
+        '@alias': '../img',
+      },
+    },
   },
   markdown: {
     extendMarkdown: md => {
-      md.set({ html: true })
-      md.use(require('markdown-it-katex'))
-      md.use(require('markdown-it-plantuml'))
-      md.use(require('markdown-it-admonition'))
-    }
-  }
-}
+      md.set({ html: true });
+      md.use(require('markdown-it-katex'));
+      md.use(require('markdown-it-plantuml'));
+      md.use(require('markdown-it-admonition'));
+    },
+  },
+};
